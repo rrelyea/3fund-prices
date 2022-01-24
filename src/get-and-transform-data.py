cc437c06063a0fe004f2671584c9d76d7f06f087
Rob Relyea <rob@relyeas.net>
2022-01-24 13:07:56 -0800

    filter to this month days only

--- src/get-and-transform-data.py
+++ src/get-and-transform-data.py
@@ -28,9 +28,14 @@ def createDataFile(ticker, function):
   del data["5. volume"]
   data.rename(columns = {'4. close':'close'}, inplace = True)
   if function == "D":
-    currentMonth = date.month
+    startOfMonth = datetime.today().replace(day=1)
+    if datetime.today().month == 12:
+      startOfNextMonth = startOfMonth.replace(month=1,year=datetime.today().year + 1)
+    else:
+      startOfNextMonth = startOfMonth.replace(month=startOfMonth.month+1)
     data['date'] = pd.to_datetime(data['date'], format='%Y-%m-%d')
-    data = data.loc[data['date'].dt.month == currentMonth]
+    data = data.query("date >= '" + str(startOfMonth) + "' \
+                      and date < '" + str(startOfNextMonth) + "'")
   data.to_csv(path, index=False)
 
 targetPath = './data/'

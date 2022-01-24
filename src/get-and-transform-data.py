a74d7acdd0bfcbcb9fbc48f259a4ce524b3d66ba
Rob Relyea <rob@relyeas.net>
2022-01-24 08:51:13 -0800

    use key

--- src/get-and-transform-data.py
+++ src/get-and-transform-data.py
@@ -1,2 +1,18 @@
 import pandas as pd
 from alphaVantageAPI import AlphaVantage
+import sys
+
+if sys.argv >= 3 and sys.argv[1] == "--key":
+  key = sys.argv[2]
+
+av = AlphaVantage(
+        api_key=key,
+        premium=False,
+        output_size="compact",
+        datatype='json',
+        export=False,
+        export_path="~/av_data",
+        output="csv",
+        clean=False,
+        proxy={}
+    )

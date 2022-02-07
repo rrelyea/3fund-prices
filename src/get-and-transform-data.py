4d91ede78545bc6e49fc346ee20547ed8b63b2ad
Rob Relyea <rob@relyeas.net>
2022-02-07 08:30:27 -0800

    include 1st of month. Fixes: https://github.com/rrelyea/3fund-prices/issues/2

--- src/get-and-transform-data.py
+++ src/get-and-transform-data.py
@@ -34,12 +34,14 @@ def updateData(ticker, function):
 
   if function == "D":
     startOfMonth = datetime.today().replace(day=1)
+    endOfLastMonth = startOfMonth - datetime.timedelta(days=1)
+
     if datetime.today().month == 12:
       startOfNextMonth = startOfMonth.replace(month=1,year=datetime.today().year + 1)
     else:
       startOfNextMonth = startOfMonth.replace(month=startOfMonth.month+1)
     data['date'] = pd.to_datetime(data['date'], format='%Y-%m-%d')
-    data = data.query("date >= '" + str(startOfMonth) + "' \
+    data = data.query("date > '" + str(endOfLastMonth) + "' \
                       and date < '" + str(startOfNextMonth) + "'")
   
   num_rows = data.count()[0]
